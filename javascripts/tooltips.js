ba66bb04c657681d3a601a18eddfde6ea1f3b4c8
Junhe Zhang <h3u8y4n6@gmail.com>
2022-01-11 19:21:09 -0500

    first commit portfolio

---
+++ javascripts/tooltips.js
@@ -0,0 +1,10 @@
+// Function to add tooltips to anchors
+console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+var anchors = document.querySelectorAll('.anchor-tooltips');
+anchors.forEach(function(anchor) {
+    var tooltipText = anchor.getAttribute('title'),
+        tooltip = document.createElement('span');
+    tooltip.className = "title-tooltip";
+    tooltip.innerHTML = tooltipText;
+    anchor.appendChild(tooltip);
+});
